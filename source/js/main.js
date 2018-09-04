21655f0d95a935af2a9426f92f602cbcb8bd599f
Ulia Usalko <usalko.ulia@gmail.com>
2018-09-04 17:20:31 +0300

    Работа над таблеткой

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,14 @@
-console.log('ololo');
+var header = document.querySelector(".header__navigation-mask");
+var header_toggle = document.querySelector(".toggle__label");
+
+header.classList.remove("header__navigation-mask--active");
+
+header_toggle.addEventListener("click", function(evt) {
+  // console.log(111)
+  // evt.stopPropagation();
+  if (header.classList.contains("header__navigation-mask--active")) {
+    header.classList.remove("header__navigation-mask--active");
+  } else {
+    header.classList.add("header__navigation-mask--active");
+  }
+});

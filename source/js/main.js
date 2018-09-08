c79774f5b2779ece77178ce19cbdf1effc2ee97e
Ulia Usalko <usalko.ulia@gmail.com>
2018-09-08 22:44:37 +0300

    Добавила блок видео

--- source/js/main.js
+++ source/js/main.js
@@ -12,3 +12,17 @@ header_toggle.addEventListener("click", function(evt) {
     header.classList.add("header__navigation-mask--active");
   }
 });
+
+var modal_button = document.querySelector(".video__button");
+var modal = document.querySelector(".video__modal");
+var modal_close_button = document.querySelector(".video__close-button");
+
+modal_button.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modal.classList.add("video__modal--active");
+});
+
+modal_close_button.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modal.classList.remove("video__modal--active");
+});
